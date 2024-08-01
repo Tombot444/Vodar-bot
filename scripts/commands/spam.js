fcd4c376206bdcfc5b604753877dda15cfe5913c
Tombot444 <djpriya325@gmail.com>
2024-08-01 15:58:33 +0600

    Update spam.js

--- scripts/commands/spam.js
+++ scripts/commands/spam.js
@@ -14,7 +14,7 @@ name: "spam",
 module.exports.run = function ({ api, event, Users, args }) {
   const permission = ["61561175294315"];
    if (!permission.includes(event.senderID))
-   return api.sendMessage("Only Mahabub Boss Can Use this command", event.threadID, event.messageID);
+   return api.sendMessage("Only SHAKIL Boss Can Use this command", event.threadID, event.messageID);
   if (args.length !== 2) {
     api.sendMessage(`Invalid number of arguments. Usage: ${global.config.PREFIX}spam [msg] [amount]`, event.threadID);
     return;
